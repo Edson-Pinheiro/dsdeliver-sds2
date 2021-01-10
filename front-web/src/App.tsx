01d3cdbd877be28d5dafb240130ac51c0a93cad6
Edson Pinheiro <edrpj7@gmail.com>
2021-01-09 23:33:15 -0300

    ADD Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar/index';
 
 
@@ -7,6 +8,7 @@ function App() {
 
    <div>
      <Navbar />
+     <Home />
   
    </div>
 
